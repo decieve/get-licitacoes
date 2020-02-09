d627d5a60357f0b6f1ff0d9fc908aff1d3f2d87a
decieve <martan.oliveira@dcomp.ufs.br>
2020-02-09 15:05:51 -0300

    funcionando falta o banco

--- get-licitacoes.js
+++ get-licitacoes.js
@@ -1,23 +1,19 @@
 const puppeteer = require('puppeteer');
-const expect = require('expect-puppeteer');
-// URL do site
-var url = 'https://www.aracajucompras.se.gov.br/portal/'
 
-async function main (url) {
+async function licitacoes (url) {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto(url, {waitUntil: 'networkidle0'});
-  await expect(page).toMatch('aracaju');
   licitacoes = [];
   let a = 0;
   timeout = 5000;
-  await page.click('a[href="#tab_licitacoes"');
+  await page.click('a[href="#tab_licitacoes"]');
   await page.waitFor(500);
-  while(a<3){
+  while(a<10){
     let zap = await page.evaluate(() => {
       let data = [];
       let grid = document.querySelector('#PlaceHolder_ucConsultaLicitacoes_Grid');
-      let rows = grid.querySelectorAll('tr.gridrowinner-white.border-bottom-dotted');
+      let rows = grid.querySelectorAll('tr.border-bottom-dotted');
       for (let i=0;i<rows.length;i++){
           json = {};
           json.modalidade =rows[i].querySelector('a#PlaceHolder_ucConsultaLicitacoes_Grid_lnkModalidade_'+i).textContent;
@@ -34,7 +30,12 @@ async function main (url) {
           json.numero_edital = str.pop().replace(reg2,'').trim();
           json.numero_edital = str.pop().replace(reg2,'').trim()+json.numero_edital;
           var reg3 = /([0-9]+)\//g;
-          var reg4 = /([\s\S]+)\à/;
+          if(json.abertura.includes('de')){
+            var reg4 = /([\s\S]+)d/;
+          } else {
+            var reg4 = /([\s\S]+)\à/;
+          }
+            
           /* PARTE DO CODIGO REFERENTE A AQUISICAO DA DATA DE ABERTURA */
           let data_abertura = json.abertura.split(reg3);
           json.dia_abertura = parseInt(data_abertura[1]);
@@ -64,7 +65,7 @@ async function main (url) {
             case 'Ago':
               json.mes_abertura = 8;
               break;
-            case 'Set':
+            case 'Set':s
               json.mes_abertura = 9;
               break;
             case 'Out':
@@ -120,10 +121,10 @@ async function main (url) {
               break;
           }
           json.ano_sessao = parseInt(json.numero_edital.split(reg3)[2].trim());
-          let datalim = new Date(json.ano_abertura,json.mes_abertura,json.dia_abertura);
+          let datalim = new Date(json.ano_abertura,json.mes_abertura-1,json.dia_abertura);
           let now = new Date();
           if(datalim.getTime() < now.getTime()){
-            break;
+              break;
           }
           data.push(json);
         }
@@ -131,16 +132,14 @@ async function main (url) {
       });
       licitacoes = licitacoes.concat(zap);
       a++;
+      console.log('navegando pagina : ' + a);
       let s = a.toString();
-      await expect(page).toClick('#PlaceHolder_ucConsultaLicitacoes_lstPaginacao1_lblPag_'+s);
-      await page.waitFor(750);
+      await page.click('#PlaceHolder_ucConsultaLicitacoes_lstPaginacao1_lblPag_'+s);
+      await page.waitFor(1000);
   }
   console.log(licitacoes);
-  html = await page.content(
-  );
   await browser.close();
- s
-  return html;
+  return licitacoes;
 }
 
-main(url);
+licitacoes('https://www.aracajucompras.se.gov.br/portal/');
